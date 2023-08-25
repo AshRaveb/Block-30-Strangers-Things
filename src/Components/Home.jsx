d7d72741cbeef6f8c301dfe5a6a76f455c24e50f
dhanajeane79 <dhanajeane79@gmail.com>
2023-08-25 15:17:41 -0400

    Added Home Page Images

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,21 +8,24 @@ function Home() {
   return (
     <div className="home-container">
       <header className="home-header">
-        <h1>Welcome to Strangers Things</h1>
+        <h1>Welcome to Stranger's Things</h1>
         <p>Your go-to platform for buying and selling unique items!</p>
       </header>
       <section className="home-features">
         <div className="feature">
           <h2>Explore</h2>
           <p>Discover a wide range of items from different sellers.</p>
+          <img className="home-image" src="src\assets\hand-explore.png" alt="Eplore" />
         </div>
         <div className="feature">
           <h2>Sell</h2>
           <p>List your own items for sale and connect with potential buyers.</p>
+          <img className="home-image" src="src\assets\sell-hand.png" alt="Sell" />
         </div>
         <div className="feature">
           <h2>Connect</h2>
           <p>Communicate with buyers and sellers through our messaging system.</p>
+          <img className="home-image" src="src\assets\connect-hand.png" alt="Connect" />
         </div>
       </section>
 <div className= "large-logo">
